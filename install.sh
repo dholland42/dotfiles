a04dab1884260c1924180d366bb11290a57d070e
Daniel Holland <danielholland414@gmail.com>
2021-05-18 21:36:16 -0400

    install script with ln -s

---
+++ install.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# DOTENV=`pwd`
+# echo "export DOTENV=${DOTENV}" >> ~/.profile
+# echo "export DOTENV=${DOTENV}" >> ~/.zprofile
+ln -s `pwd`/.hyper.js ~/.hyper.js
+ln -s `pwd`/.tmux.conf ~/.tmux.conf
+ln -s `pwd`/.tmux ~/.tmux
